642d5d69a8897b96995f90f560fc87c86f0dac71
zhaochanjuan <840875609@qq.com>
2018-09-17 16:28:25 +0800

    【赵婵娟】提交公共图片

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+	<div id="app">
+		<Appheader></Appheader>
+		<Inspec></Inspec>
+		<router-view/>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+	import Header from './components/Header'
+	import Inspec from './pages/Inspection'
+	export default {
+		name: 'App',
+		components: {
+			'Appheader': Header,
+			'Inspec':Inspec
+		}
+	}
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ 
+  *{
+  	margin: 0px;
+    padding: 0px;
+  }
+    body{
+    	background:#fafdff;
+    }
+	#app {
+		/*font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;*/
+	}
 </style>
